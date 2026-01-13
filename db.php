71c8e6e58e3e42aba4b8b29a6ef7e6faa509be17
dilumgit <dilkaru999@gmail.com>
2026-01-13 23:06:34 +0530

    updated the db file

--- db.php
+++ db.php
@@ -1,10 +1,11 @@
 <?php
-$host = "localhost";
-$user = "root";
-$password = "";
-$database = "construction_site";
+$host = "sql12.freesqldatabase.com";
+$user = "sql12814242";
+$password = "EX4QU5NKn7";
+$database = "sql12814242";
+$port = 3306;
 
-$conn = mysqli_connect($host, $user, $password, $database);
+$conn = mysqli_connect($host, $user, $password, $database, $port);
 
 if (!$conn) {
     die("Database connection failed: " . mysqli_connect_error());
